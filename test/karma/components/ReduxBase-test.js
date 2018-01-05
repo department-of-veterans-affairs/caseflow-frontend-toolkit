a0dc29e87f4ab35bfabbf364934770f9d924df02
Nick Heiner <nth23@cornell.edu>
2018-01-05 15:42:37 -0500

    trying to get karma tests going

---
+++ test/karma/components/ReduxBase-test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { connect } from 'react-redux';
+
+import ReduxBase from '../../../components/ReduxBase';
+
+const initialState = {
+  key: 'initial value'
+};
+
+const ACTION_NAME = 'ACTION_NAME';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case ACTION_NAME:
+    return { key: 'updated value' };
+  default:
+    return state;
+  }
+};
+
+const keyId = 'redux-key';
+
+class ReduxDisplay extends React.PureComponent {
+  render() {
+    return <span id={keyId}>{this.props.key}</span>;
+  }
+}
+
+const ConnectedReduxDisplay = connect(
+  (state) => state
+)(ReduxDisplay);
+
+class TestHarness extends React.PureComponent {
+  render() {
+    return <ReduxBase initialState={initialState} reducer={reducer}>
+      <ConnectedReduxDisplay />
+    </ReduxBase>;
+  }
+}
+
+describe('ReduxBase', () => {
+  it('creates a working Redux environment', () => {
+    const elem = mount(TestHarness);
+
+    expect(elem.find(`#${keyId}`)).to.equal('initial value');
+  });
+});
---
+++ test/karma/test-index.js
@@ -0,0 +1,23 @@
+import enzyme from 'enzyme';
+import EnzymeAdapterReact16 from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new EnzymeAdapterReact16() });
+
+// TODO let's DRY this out with Caseflow.
+// eslint-disable-next-line no-empty-function
+window.analyticsPageView = function() {
+};
+
+// eslint-disable-next-line no-empty-function
+window.analyticsEvent = function() {
+};
+
+// eslint-disable-next-line no-empty-function
+window.analyticsTiming = function() {
+};
+
+// require all modules ending in "-test" from the
+// current directory and all subdirectories
+const testsContext = require.context('.', true, /-test.js$/);
+
+testsContext.keys().forEach(testsContext);
