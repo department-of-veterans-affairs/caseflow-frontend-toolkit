eefe4ebd213bf5ac09247372de27c71d9b383ac1
Nick Heiner <nth23@cornell.edu>
2018-01-08 17:28:35 -0500

    use bootstrap file

---
+++ test/bootstrap.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-empty-function
+window.analyticsPageView = () => {};
+
+// eslint-disable-next-line no-empty-function
+window.analyticsEvent = () => {};
+
+// eslint-disable-next-line no-empty-function
+window.analyticsTiming = () => {};
--- test/karma/test-index.js
+++ test/karma/test-index.js
@@ -6,18 +6,7 @@ import chai from 'chai';
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
-// TODO let's DRY this out with Caseflow.
-// eslint-disable-next-line no-empty-function
-window.analyticsPageView = function() {
-};
-
-// eslint-disable-next-line no-empty-function
-window.analyticsEvent = function() {
-};
-
-// eslint-disable-next-line no-empty-function
-window.analyticsTiming = function() {
-};
+import '../bootstrap';
 
 // require all modules ending in "-test" from the
 // current directory and all subdirectories
