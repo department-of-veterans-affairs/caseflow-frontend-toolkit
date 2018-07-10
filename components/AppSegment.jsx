789934a3b28b34edd7f77a070aa6f00576a87151
Nolawi Petros <nolawi.petros@gmail.com>
2018-07-10 16:28:17 -0400

    main section

--- components/AppSegment.jsx
+++ components/AppSegment.jsx
@@ -12,11 +12,11 @@ const AppSegment = ({ children, extraClassNames, filledBackground, noMarginTop, 
     css({ marginTop: '0 !important' }) :
     {};
 
-  return <div {...marginTopStyling} {...styling} className={classNames('cf-app-segment', extraClassNames, {
+  return <section {...marginTopStyling} {...styling} role="main" className={classNames('cf-app-segment', extraClassNames, {
     'cf-app-segment--alt': filledBackground
   })}>
     {children}
-  </div>;
+  </section>;
 };
 
 export default AppSegment;
