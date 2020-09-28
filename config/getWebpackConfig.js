9c5818067628e3f8c753e8de2daffe65829b24fc
JC Quirin <jcq@jcq.lan>
2020-09-28 11:41:38 -0700

    updated Node version to 12.13.0; updated all Babel packages to match main CF repo; updated Babel config to match main CF repo; updated Webpack cfg to remove deprecated Babel bits;

--- config/getWebpackConfig.js
+++ config/getWebpackConfig.js
@@ -11,7 +11,7 @@ module.exports = (callingDirname, entry) => {
     entry: _.compact([
       'es5-shim/es5-shim',
       'es5-shim/es5-sham',
-      'babel-polyfill',
+      // 'babel-polyfill',
       entry
     ]),
 
